5958f0f891021e2b0c37b13bfa7bc6fd7a146559
Sruthi-Palle <pallesruthi12@gmail.com>
2023-01-03 16:39:46 +0530

    added

---
+++ src/components/EditModal.jsx
@@ -0,0 +1,106 @@
+export const EditModal = ({
+  setShowModal,
+  form,
+  setItems,
+  setForm,
+  item,
+  id,
+  intialForm,
+  items,
+}) => {
+  let editItem = items.find((el) => {
+    return el.id === id;
+  });
+  console.log(editItem);
+  setForm(editItem);
+  const handleInput = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+    console.log(form);
+  };
+  const addSave = () => {
+    setItems([...items, form]);
+    setForm(intialForm);
+    console.log(items);
+    setShowModal(false);
+  };
+  return (
+    <div>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl font-semibold">Task</h3>
+              <button
+                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={() => setShowModal(false)}
+              >
+                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            {/*body*/}
+            <div className="relative p-6 flex-auto">
+              <label
+                htmlFor="title"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Title
+              </label>
+              <textarea
+                id="title"
+                name="title"
+                value={form.title}
+                rows="4"
+                onChange={handleInput}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              ></textarea>
+
+              <br />
+
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Description
+              </label>
+              <textarea
+                name="description"
+                id="description"
+                value={form.description}
+                onChange={handleInput}
+                rows="4"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              ></textarea>
+            </div>
+            {/*footer*/}
+            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+              <button
+                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(false)}
+              >
+                Close
+              </button>
+              <button
+                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={addSave}
+              >
+                Save Changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </div>
+  );
+};
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { AddModal } from "./AddModal";
+import { EditModal } from "./EditModal";
 
 export const Timer = () => {
   let now = new Date().toLocaleTimeString();
+  let intialForm = { id: "", title: "", description: "" };
+  const [form, setForm] = useState(intialForm);
+  const [edit, setEdit] = useState(false);
+  const [items, setItems] = useState([]);
   const [startTimer, setStartTimer] = useState(false);
   const [ctime, setCtime] = useState(now);
+  const [showModal, setShowModal] = useState(false);
   const updateTime = () => {
     let time = new Date().toLocaleTimeString();
     setCtime(time);
@@ -22,8 +31,60 @@ export const Timer = () => {
   return (
     <div>
       <h1>{ctime}</h1>
-      <button onClick={() => setStartTimer(true)}>start</button>
-      <button onClick={() => setStartTimer(false)}>stop</button>
+      <button className="bg-slate-300 m-2" onClick={() => setStartTimer(true)}>
+        start
+      </button>
+      <button className="bg-slate-300 m-2" onClick={() => setStartTimer(false)}>
+        stop
+      </button>
+      <button className="bg-slate-300 m-2" onClick={() => setShowModal(true)}>
+        save
+      </button>
+      {showModal ? (
+        <AddModal
+          setShowModal={setShowModal}
+          setForm={setForm}
+          setItems={setItems}
+          form={form}
+          items={items}
+          intialForm={intialForm}
+        />
+      ) : (
+        ""
+      )}
+      <br />
+      {items.map((item, i) => {
+        return (
+          <div className="flex justify-center items-center gap-5" key={item.id}>
+            <div>
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+            <div className="">
+              <button onClick={() => setEdit(true)}>
+                <EditIcon />
+              </button>
+              <button>
+                <DeleteIcon />
+              </button>
+            </div>
+            {edit ? (
+              <EditModal
+                id={item.id}
+                setShowModal={setShowModal}
+                setForm={setForm}
+                setItems={setItems}
+                form={form}
+                items={items}
+                intialForm={intialForm}
+                item={item}
+              />
+            ) : (
+              ""
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
